bf26fe458c265af67c791382c9aa6e74edc962cb
jabeca86 <42305606+jabeca86@users.noreply.github.com>
2025-01-11 01:48:15 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tendancys Test App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <small>by Jacobo Alejandro Ben&iacute;tez Canavati</small>
       </header>
     </div>
   );
