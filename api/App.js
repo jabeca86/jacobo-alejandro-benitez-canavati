9e80062406aa97fe9a338b6750c064344ac5c78f
jabeca86 <42305606+jabeca86@users.noreply.github.com>
2025-01-14 10:36:46 -0600

    WIP: API (productRoute)

--- api/App.js
+++ api/App.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const exampleRoute = require('./routes/exampleRoute');
+const exampleRoute = require('./routes/productRoute');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use('/api', exampleRoute);
+app.use('/api', productRoute);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
