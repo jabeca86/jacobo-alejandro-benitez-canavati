080fd7173d4b797da226f66510851c690f8c70ba
jabeca86 <42305606+jabeca86@users.noreply.github.com>
2025-01-19 17:34:13 -0600

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const exampleRoute = require('./routes/productRoute');
+const productRoute = require('./routes/productRoute');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
